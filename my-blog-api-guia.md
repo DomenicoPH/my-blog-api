e9eafeab8e8be6751934b07ee7f1fc24da2daad7
Domenico Pagano <dpaganoh@gmail.com>
2025-08-28 21:29:56 -0500

    @Put + exception filters

--- my-blog-api-guia.md
+++ my-blog-api-guia.md
@@ -1,5 +1,6 @@
 ## NestJS
 - [NestJS Docs - First Steps](https://docs.nestjs.com/first-steps)
+- [NestJS Exception Filters](https://docs.nestjs.com/exception-filters)
 
 ## Comandos de inicio
 
@@ -27,3 +28,31 @@
 [POST] https://localhost:3001/users         --> Retorna el usuario creado (201)
 
 [DELETE] https://localhost:3001/users/:id   --> Retorna status de usuario borrado (200)
+
+[PUT] https://localhost:3001/users/:id      --> Retorna el usuario actualizado (200)
+
+## Exception Filters
+
+- **400** - BadRequestException
+- **401** - UnauthorizedException
+- **404** - NotFoundException
+- **403** - ForbiddenException
+- **406** - NotAcceptableException
+- **408** - RequestTimeoutException
+- **409** - ConflictException
+- **410** - GoneException
+- **505** - HttpVersionNotSupportedException
+- **413** - PayloadTooLargeException
+- **415** - UnsupportedMediaTypeException
+- **422** - UnprocessableEntityException
+- **500** - InternalServerErrorException
+- **501** - NotImplementedException
+- **418** - ImATeapotException
+- **405** - MethodNotAllowedException
+- **502** - BadGatewayException
+- **503** - ServiceUnavailableException
+- **504** - GatewayTimeoutException
+- **512** - PreconditionFailedException
+
+## Enlaces
+[Status Codes](https://en.wikipedia.org/wiki/List_of_HTTP_status_codes)
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, Param, Post, Body, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Delete,
+  Put,
+  NotFoundException,
+  UnprocessableEntityException,
+  ForbiddenException
+} from '@nestjs/common';
 
 interface User {
   id: string;
@@ -9,10 +20,7 @@ interface User {
 @Controller('users')
 export class UsersController {
   private users: User[] = [
-    { id: '1', name: 'Domenico Pagano', email: 'domenico@example.com' },
-    { id: '2', name: 'Juan Perez', email: 'juan@example.com' },
-    { id: '3', name: 'Maria Gomez', email: 'maria@example.com' },
-    { id: '4', name: 'Gnomo No', email: 'gnomono@example.com' },
+    { id: '1', name: 'Domenico Pagano', email: 'master_user@example.com' },
   ];
 
   @Get()
@@ -24,7 +32,10 @@ export class UsersController {
   findUser(@Param('id') id: string) {
     const user = this.users.find(user => user.id === id);
     if(!user){
-      return { error: 'User not found' };
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+    if(user.id === '1'){
+      throw new ForbiddenException(`User with id ${id} is not allowed to access this resource`);
     }
     return user;
   }
@@ -37,9 +48,18 @@ export class UsersController {
   }
   */
 
+  /* Método this.users.length + 1
   @Post()
   createUser(@Body() body: User) {
     const newUser = { ...body, id: (this.users.length + 1).toString() };
+    this.users.push(newUser);
+    return newUser;
+  }
+  */
+
+  @Post()
+  createUser(@Body() body: User) {
+    const newUser = { ...body, id: new Date().getTime().toString() };
     this.users.push(newUser);
     return newUser;
   }
@@ -60,10 +80,40 @@ export class UsersController {
   deleteUser(@Param('id') id: string) {
     const position = this.users.findIndex( user => user.id === id);
     if(position === -1){
-      return { error: 'User not found' };
+      throw new NotFoundException(`User with id ${id} not found`);
     }
     this.users.splice(position, 1);
     return { message: 'User deleted' };
+  }
+
+  /*
+  @Put(':id')
+  updateUser(@Param('id') id: string, @Body() body: Partial<User>) {
+    const user = this.users.find(user => user.id === id);
+    if(!user){
+      return { error: 'User not found' };
+    }
+    Object.assign(user, body);
+    return user;
+  }
+  */
+
+  @Put(':id')
+  updateUser(@Param('id') id: string, @Body() changes: User){
+    const position = this.users.findIndex( user => user.id === id);
+    if(position === -1){
+      return { error: 'User not found' };
+    }
+    const currentData = this.users[position];
+
+    const email = changes?.email;
+    if(email && !email.includes('@')){
+      throw new UnprocessableEntityException(`Invalid email format: ${email}`);
+    }
+
+    const updatedUser = { ...currentData, ...changes };
+    this.users[position] = updatedUser;
+    return updatedUser;
   }
 
 };
